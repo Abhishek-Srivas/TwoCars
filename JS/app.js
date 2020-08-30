42c1d696c1139b9412c9acfbec21a2c9d1087e86
Abhishek Srivas <abhiautar123@gmail.com>
2020-08-30 13:58:47 +0530

    restart menu added,start game functionality added

--- JS/app.js
+++ JS/app.js
@@ -1,15 +1,17 @@
 //commonly used properties are defined for shortcut
-var width_ = window.innerWidth;
-var height_ = window.innerHeight;
+var width = window.innerWidth;
+var height = window.innerHeight;
 var canvas = document.getElementById("canvas");
 var c = canvas.getContext('2d');
-canvas.width = width_;
-canvas.height = height_;
+canvas.width = width;
+canvas.height = height;
 canvas.style.backgroundColor = "#25337a";
 c.fillStyle = "grey"; //line color
 
+
+
 function clearCanvas() {
-    c.clearRect(0, 0, width_, height_);
+    c.clearRect(0, 0, width, height);
 }
 var redcar = new Image(); // Create new img element
 var bluecar= new Image();  
@@ -19,17 +21,25 @@ bluecar.src="../Images/Blue.png";
 window.onload = function () //onload is used to draw when image has loaded
 {   
     //to draw a line passed x,y,width,height
-    c.fillRect(width_/2,0,2,height_);
-    c.fillRect(width_/2 - 100,0,2,height_);
-    c.fillRect(width_/2-200,0,2,height_);
-    c.fillRect(width_/2+100,0,2,height_);
-    c.fillRect(width_/2+200,0,2,height_);
+    c.fillStyle = "#fff";
+    c.fillRect(width/2,0,2,height);
+    c.fillRect(width/2 - 100,0,2,height);
+    c.fillRect(width/2-200,0,2,height);
+    c.fillRect(width/2+100,0,2,height);
+    c.fillRect(width/2+200,0,2,height);
     //draw image function name coordinates
-    c.drawImage(redcar,width_/2 - 180,height_-180);
-    c.drawImage(bluecar,width_/2 +120,height_-180);
+    c.drawImage(redcar,width/2 - 180,height-180);
+    c.drawImage(bluecar,width/2 +120,height-180);
 };
 
 /*
 fillrect refernce from http://drawingincode.com/lessons/reference/fill_rect/index.html
 */
+
+
+// to hide the start screen while playing
+function playbutton(){
+      document.querySelector('#startMenu').style.display = "none";
+}
+
 
